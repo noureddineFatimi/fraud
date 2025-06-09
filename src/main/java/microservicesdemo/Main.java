cb0f588d6f74107df78918a540410f2e87a0b50c
Noureddine El Fatimi <nourddineeagles@gmail.com>
2025-06-09 23:19:03 +0100

    fraud

--- src/main/java/microservicesdemo/Main.java
+++ src/main/java/microservicesdemo/Main.java
@@ -2,10 +2,10 @@ package microservicesdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
